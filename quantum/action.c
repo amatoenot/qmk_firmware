3c8f889216f21c4873f3b8f63c061ea8cd7c3fee
amatoenot <avatarcanbedj@yandex.ru>
2024-02-27 18:03:47 +0400

    Add temp workaround to prevent shifted keys mod bleed

--- quantum/action.c
+++ quantum/action.c
@@ -81,10 +81,10 @@ void action_exec(keyevent_t event) {
 #endif
     }
 
-    if (event.pressed) {
-        // clear the potential weak mods left by previously pressed keys
-        clear_weak_mods();
-    }
+    // if (event.pressed) {
+    //     // clear the potential weak mods left by previously pressed keys
+    //     clear_weak_mods();
+    // }
 
 #ifdef SWAP_HANDS_ENABLE
     // Swap hands handles both keys and encoders, if ENCODER_MAP_ENABLE is defined.
@@ -367,6 +367,12 @@ void process_action(keyrecord_t *record, action_t action) {
 #ifndef NO_ACTION_TAPPING
     uint8_t tap_count = record->tap.count;
 #endif
+
+    if (event.pressed) {
+        // clear the potential weak mods left by previously pressed keys
+        clear_weak_mods();
+    }
+
 
 #ifndef NO_ACTION_ONESHOT
     bool do_release_oneshot = false;
